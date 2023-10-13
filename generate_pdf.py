e702e4f54e7d5699a68b1eca0a8a35ec83e9aa9c
Mirko Westermeier <mirko.westermeier@uni-muenster.de>
2023-10-13 13:35:45 +0200

    Generate PDFs with target name given as CLA

--- generate_pdf.py
+++ generate_pdf.py
@@ -1,61 +1,58 @@
 import sys
-import re
 from configparser import ConfigParser
 from fpdf import FPDF
 
-# Read configuration
+# Read configuraton
 config = ConfigParser()
 config.read('config.ini')
 
-# Convert unit strings to values
-def parse_strips(s):
-    factor = float(config['geometry']['default_unit'])
-    return list(map(lambda s: factor * float(s), s.split(' ')))
+# Read target name from command line
+if len(sys.argv) != 2:
+  sys.exit('Target name expected as the program\'s (only) argument')
+name = sys.argv[1]
+if name not in config['strips']:
+  sys.exit(f'Unknown target name: "{name}"')
 
-# Process strip data
-for name, numbers in config['strips'].items():
+file_name = f'output/pdf/{name}.pdf'
+print(f'Writing {name} to {file_name}...', end='')
 
-    # Log
-    print(f'Writing {name}.pdf:', end = '')
+# Calculate strips
+widths    = config['strips'][name]
+factor    = float(config['geometry']['default_unit'])
+strips    = [factor * float(s) for s in widths.split(' ')]
+thickness = sum(strips)
 
-    # Prepare PDF
-    pdf = FPDF(
-        orientation = config['papersize']['orientation'],
-        format      = config['papersize']['name'],
-        unit        = 'mm',
-    )
-    pdf.add_page()
-    pdf.set_fill_color(0, 0, 0)
+# Prepare PDF
+paper = config['papersize']
+pdf = FPDF(
+  orientation = paper['orientation'],
+  format      = paper['name'],
+  unit        = 'mm',
+)
+pdf.add_page()
+pdf.set_fill_color(0, 0, 0)
 
-    # Parse data
-    strips      = parse_strips(numbers)
-    thickness   = sum(strips)
+# Draw lines
+start = (float(paper['width']) - thickness) / 2
+black = True
+y     = 0
+for th in strips:
+  if black:
+    pdf.rect(
+      x=0, y=start+y, h=th, style='F',
+      w=float(paper['height']))
+  y += th
+  black = not black
+  print(' ' + str(th), end = '')
 
-    # Draw lines
-    start = (float(config['papersize']['width']) - thickness) / 2
-    black = True
-    y     = 0
-    for th in strips:
-        if (black):
-            pdf.rect(
-                x       = 0,
-                y       = start+y,
-                w       = float(config['papersize']['height']),
-                h       = th,
-                style   = 'F',
-            )
-        y += th
-        black = not(black)
-        print(' ' + str(th), end = '')
+# Add target name
+pdf.set_font('Helvetica', 'B', 8)
+pdf.text(x=20, y=start+y+10, txt=name)
 
-    # Add target name
-    pdf.set_font('Helvetica', 'B', 8)
-    pdf.text(x = 20, y = start + y + 10, txt = name)
+# Add arrow, if neccessary
+if name == 'finish':
+  pdf.text(x=17, y=start+y+10, txt='^')
 
-    # Add arrow, if neccessary
-    if name == 'finish':
-        pdf.text(x = 17, y = start + y + 10, txt = '^')
-
-    # Write to file
-    pdf.output(f'output/pdf/{name}.pdf')
-    print(' ...done.')
+# Write to file
+pdf.output(file_name)
+print(' ... done.')
